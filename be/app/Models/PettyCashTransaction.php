262565ccb5b9de76e2208c27be0d9c17a09c5555
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2026-08-09 15:51:26 +0700

    fix(model): remove non-existent LogsActivity trait from PettyCashTransaction

--- be/app/Models/PettyCashTransaction.php
+++ be/app/Models/PettyCashTransaction.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Traits\LogsActivity;
 
 class PettyCashTransaction extends Model
 {
-    use HasFactory, SoftDeletes, LogsActivity;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'petty_cash_transactions';
 
